ad39330e51adb06edfc03aadee70885c440e7c18
sumitinvisible <sumitranchi003@gmail.com>
2017-05-17 14:33:53 +0530

    Add files via upload

---
+++ emp.js
@@ -0,0 +1,12 @@
+var emp = {
+    "name" : "Sivapriya",
+        "age" : 28,
+            "salary" : 18800,
+                "address" : {
+                    "city" : "Bangalore","State" : "Karnataka","Pin" : 560048
+                        
+                            
+                },
+                };  
+alert("Nested Object");
+console.log(emp);
